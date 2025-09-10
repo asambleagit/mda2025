14c5041a66816d888a4d87cda38f0164e609d3a9
asambleagit <asambleagit@gmail.com>
2025-09-10 18:15:35 -0300

    Seccion Servicios

---
+++ servicios.js
@@ -0,0 +1,23 @@
+export const servicios = [{
+    categoria: "farmacia",
+    icon: "fa-home",
+    localidad: "bernardo",
+    titulo: "Farmacias en San Bernardo",
+    link1: "https://lacosta.gob.ar/salud/",
+},
+{
+    categoria: "hotel",
+    icon: "fa-home",
+    localidad: "bernardo",
+    titulo: "Hoteles en San Bernardo",
+    link1: "https://lacosta.gob.ar/salud/",
+},
+{
+    categoria: "farmacia",
+    icon: "fa-home",
+    localidad: "ajo",
+    titulo: "Farmacias en Mar de Ajó",
+    link1: "https://lacosta.gob.ar/salud/",
+},
+
+]
