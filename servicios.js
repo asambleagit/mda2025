61f0e32a7588f0f47ae95f987a7adc3140cedff6
goga22 <gabriel.o.garcia@gmail.com>
2025-09-12 22:17:16 -0300

    Agregar archivos de configuración y estilos para la sección de servicios, incluyendo la lógica de carga de datos desde un archivo JSON.

--- servicios.js
+++ servicios.js
@@ -1,20 +1,17 @@
 export const servicios = [{
     categoria: "farmacia",
-    icon: "fa-home",
     localidad: "bernardo",
     titulo: "Farmacias en San Bernardo",
     link1: "https://lacosta.gob.ar/salud/",
 },
 {
     categoria: "hotel",
-    icon: "fa-home",
     localidad: "bernardo",
     titulo: "Hoteles en San Bernardo",
     link1: "https://lacosta.gob.ar/salud/",
 },
 {
     categoria: "farmacia",
-    icon: "fa-home",
     localidad: "ajo",
     titulo: "Farmacias en Mar de Ajó",
     link1: "https://lacosta.gob.ar/salud/",
