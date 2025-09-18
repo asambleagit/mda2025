3ef7cc50a39d6b7a2956f25717a84c2fcfb3402a
goga22 <gabriel.o.garcia@gmail.com>
2025-09-18 20:15:00 -0300

    Agregar archivo TODO para mejorar estilos en servicios.html y actualizar servicios.js a formato JSON con íconos.

--- servicios.js
+++ servicios.js
@@ -1,20 +1,30 @@
-export const servicios = [{
-    categoria: "farmacia",
-    localidad: "bernardo",
-    titulo: "Farmacias en San Bernardo",
-    link1: "https://lacosta.gob.ar/salud/",
-},
-{
-    categoria: "hotel",
-    localidad: "bernardo",
-    titulo: "Hoteles en San Bernardo",
-    link1: "https://lacosta.gob.ar/salud/",
-},
-{
-    categoria: "farmacia",
-    localidad: "ajo",
-    titulo: "Farmacias en Mar de Ajó",
-    link1: "https://lacosta.gob.ar/salud/",
-},
-
+[
+  {
+    "categoria": "farmacia",
+    "icon": "fa-prescription-bottle",
+    "localidad": "bernardo",
+    "titulo": "Farmacias en San Bernardo",
+    "link1": "https://lacosta.gob.ar/salud/"
+  },
+  {
+    "categoria": "hotel",
+    "icon": "fa-hotel",
+    "localidad": "bernardo",
+    "titulo": "Hoteles en San Bernardo",
+    "link1": "https://lacosta.gob.ar/turismo/"
+  },
+  {
+    "categoria": "farmacia",
+    "icon": "fa-prescription-bottle",
+    "localidad": "ajo",
+    "titulo": "Farmacias en Mar de Ajó",
+    "link1": "https://lacosta.gob.ar/salud/"
+  },
+  {
+    "categoria": "restaurante",
+    "icon": "fa-utensils",
+    "localidad": "ajo",
+    "titulo": "Restaurantes en Mar de Ajó",
+    "link1": "https://www.google.com/maps/search/Restaurantes/@-36.7221367,-56.7301056,9287m/data=!3m2!1e3!4b1!4m7!2m6!3m5!2sEstadio+de+La+Costa!3s0x959c6fa0c02a17eb:0x6df214c0748d4060!4m2!1d-56.6973841!2d-36.7091731?entry=ttu&g_ep=EgoyMDI1MDkwNy4wIKXMDSoASAFQAw%3D%3D"
+  }
 ]
